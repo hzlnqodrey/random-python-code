fcdc21d5249e764c45f3c23d22f9cd4c2181e759
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-30 19:33:26 +0700

    practice quiz

--- app.py
+++ app.py
@@ -32,3 +32,18 @@ tip = bill * (0.15)
 total = bill + tip
 share = total / 2 
 print("Each person needs to pay: " + str(share))
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(int(result))
+
+word1 = "How "
+word2 = "do "
+word3 = "you "
+word4 = "like "
+word5 = "Python "
+word6 = "so "
+word7 = "far?"
+
+print(word1+word2+word3+word4+word5+word6+word7)
