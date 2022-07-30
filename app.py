11c08ea1bcb4546af2932c68e72d62fe87aad340
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-30 20:51:46 +0700

    remaining seconds func

--- app.py
+++ app.py
@@ -84,3 +84,13 @@ amount_a = get_seconds(2, 30, 0)
 amount_b = get_seconds(0, 45, 15)
 result = amount_a + amount_b
 print(result)
+
+# remaining seconds func
+def convert_to_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+hours, minutes, remaining_seconds = convert_to_seconds(7841)
+print(hours, minutes, remaining_seconds)
