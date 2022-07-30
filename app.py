b36037c7266819dcbf16836355c711b2be2321f1
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-30 20:42:19 +0700

    area of triangle calc func - extras

--- app.py
+++ app.py
@@ -75,3 +75,12 @@ area_b = area_triangle(90, 8)
 print("Area B: ", area_b)
 sum_area = area_a + area_b
 print("The sum of areas is: ", sum_area)
+
+# seconds convertion calc return value
+def get_seconds(hours, minutes, seconds):
+  return 3600*hours + 60*minutes + seconds
+
+amount_a = get_seconds(2, 30, 0)
+amount_b = get_seconds(0, 45, 15)
+result = amount_a + amount_b
+print(result)
