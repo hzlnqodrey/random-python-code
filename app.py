71ccb659e682d27fd959ae9eabf528bc4c7a5d28
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-30 23:03:20 +0700

    format_name function

--- app.py
+++ app.py
@@ -316,6 +316,34 @@ print(exam_grade(0)) # Should be Fail
 
 #####################################################
 
+# format_name function
+
+def format_name(first_name, last_name):
+	if first_name != "" and last_name != "":
+		string = "Name: " + last_name + ", " + first_name
+		return string
+	elif first_name == "" and last_name != "":
+		string = "Name: " + last_name
+		return string
+	elif first_name != "" and last_name == "":
+		string = "Name: " + first_name
+		return string
+	else:
+		string = ""
+		return string
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
+
 #####################################################
 
 #####################################################
