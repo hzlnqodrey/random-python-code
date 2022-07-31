e4a3da15d8bde96755af6f4b581d70db5366caea
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 20:58:15 +0700

    check first and last character

--- app.py
+++ app.py
@@ -832,3 +832,16 @@ print(name[0])
 print(name[len(name)-1]) # how to search the last character in the strings
 # or use negative indexes
 print(name[-1]) # output: n
+
+def first_and_last(message):
+    if len(message) == 0:
+        return True
+    else:
+        if message[0] == message[-1]:
+            return True
+        else:
+            return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
+print(first_and_last(""))
