4549c82f7f60cb9213f123b3dc8f596db3712a0d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 12:33:35 +0700

    hop onto some lil quiz

--- app.py
+++ app.py
@@ -402,6 +402,36 @@ while x != 0 and x % 2 == 0:
     x = x / 2
 
 #####################################################
+# The following code causes an infinite loop. Can you figure out what’s missing and how to fix it?
+
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+		print(n)
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
+
+# possible solution:
+
+def print_range(start, end):
+	# Loop through the numbers from start to end
+	n = start
+	while n <= end:
+		print(n)
+		n += 1
+
+print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line) 
+
+#Here is your output:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# Great work! You've managed to fix the error in the code that
+# was causing an infinite loop!
 
 #####################################################
 
