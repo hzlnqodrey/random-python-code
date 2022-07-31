b8ea1164e57b5c584e2b6a328e1e9de694123c85
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 12:48:08 +0700

    is_power_of_two division and remainder program

--- app.py
+++ app.py
@@ -435,7 +435,46 @@ print_range(1, 5)  # Should print 1 2 3 4 5 (each number on its own line)
 
 #####################################################
 
+# the print_prime_factors function print all the prime factors of a number. A prime factor is a number that is prime and divides another without a remainder.
+
+# def print_prime_factors(number):
+#   # Start with two, which is the first prime
+#   factor = 2
+#   # Keep going until the factor is larger than the number
+#   while factor <= number:
+#     # Check if factor is a divisor of number
+#     if number % factor == 0:
+#       # If it is, print it and divide the original number
+#       print(factor)
+#       number = number / factor
+#     else:
+#       # If it's not, increment the factor by one
+#       factor += 1
+#   return "Done"
+
+# print_prime_factors(100)
+# Should print 2,2,5,5
+# DO NOT DELETE THIS COMMENT
+
 #####################################################
+
+#  is_power_of_two division and remainder program
+
+def is_power_of_two(n):
+  # Check if the number can be divided by two without a remainder
+  while n % 2 == 0 and n != 0:
+    n = n / 2
+  # If after dividing by two the number is 1, it's a power of two
+  if n == 1:
+    return True
+  return False
+  
+
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
 #####################################################
 
 
