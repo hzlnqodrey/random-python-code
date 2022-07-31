38335fa000e7196172f4abdb9afe6ae0b8ed6936
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 16:02:38 +0700

    multiple of 7

--- app.py
+++ app.py
@@ -629,3 +629,9 @@ for n in range(0,1):
 
 for x in range(1,11):
   print(x**3)
+
+# Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+
+for x in range(0, 101):
+    if x % 7 == 0:
+        print(x)
