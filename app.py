943bc185e55d9032f47b409c0b9b639a27f7b4b3
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 16:59:00 +0700

    is_power_of function recursion - Practice Quiz: Recursion Number 3

--- app.py
+++ app.py
@@ -653,3 +653,23 @@ def factorial(n):
     if n < 2: # base case
         return 1
     return n * factorial(n-1) # recursive case
+
+#####################################################
+
+
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+      if number == 1:
+        return True
+      return False
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number/base, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
+print(is_power_of(100,10)) # Should be False
+print(is_power_of(625,5)) # Should be False
