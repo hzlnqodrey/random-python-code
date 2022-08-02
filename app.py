b6c2eb0379dea59ab90846c607a261cb244c9244
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-02 14:38:22 +0700

    another example of string format in python

--- app.py
+++ app.py
@@ -947,3 +947,11 @@ print("Hello {}!. Your lucky number is {}...".format(name, number))
 
 print("Your token is {tokenizer}, {name}".format(name=name, tokenizer="tokenizer"))
 
+# another exampple
+
+def student_grade(name, grade):
+	return "{name} received {grade}% on the exam".format(name=name, grade=grade)
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
