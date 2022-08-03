24fdcd521c7b4d36dc30e439018a43bbbe4537f0
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-04 02:28:44 +0700

    PQ: List and Tuples, Q6 Solved

--- app.py
+++ app.py
@@ -1233,3 +1233,20 @@ def group_list(group, users):
 print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
 print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
 print(group_list("Users", "")) # Should be "Users:"
+
+# Question 6
+
+def guest_list(guests):
+	for guest in guests:
+		name, age, profession = guest
+		print("{} is {} years old and works as {}".format(name, age, profession))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+#Click Run to submit code
+# """
+# Output should match:
+# Ken is 30 years old and works as Chef
+# Pat is 35 years old and works as Lawyer
+# Amanda is 25 years old and works as Engineer
+# """
