49c94ba6c653ad119c03418b0cde586c043b44d8
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-04 00:14:21 +0700

    odd_numbers with list comp

--- app.py
+++ app.py
@@ -1156,3 +1156,12 @@ len_language = [len(language) for language in languages]
 
 z = [i for i in range(0, 1001) if i % 3 == 0]
 print(z)
+
+def odd_numbers(n):
+	return [x for x in range(0, n+1) if x % 2 != 0]
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
