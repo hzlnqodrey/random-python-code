93fcac2350c15b9fd1ca55df6ac066c2624eba3a
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-05 01:00:03 +0700

    Short Quiz in What is a dictionary?

--- app.py
+++ app.py
@@ -1285,3 +1285,11 @@ print(file_counts)
 # delete key in dict
 del file_counts["cfg"]
 print(file_counts)
+
+# Short Quiz
+
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+toc["Epilogue"] = 39 # Epilogue starts on page 39
+toc["Chapter 3"] = 24 # Chapter 3 now starts on page 24
+print(toc) # What are the current contents of the dictionary?
+print("Chapter 5" in toc)# Is there a Chapter 5?
