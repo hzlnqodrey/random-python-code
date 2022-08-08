c073862cc824af267d9516a52c845df2fc5d571f
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-09 00:46:03 +0700

    Module 4 Graded Assessment Q1 Done

--- app.py
+++ app.py
@@ -1564,3 +1564,30 @@ groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
 	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
 
 print(add_prices(groceries)) # Should print 28.44
+
+
+def format_address(address_string):
+  # Declare variables
+  number_address = ""
+  # Separate the address string into parts
+  split_string = address_string.split()
+  # Traverse through the address parts
+  for i in split_string:
+    # Determine if the address part is the
+    # house number or part of the street name
+    if i.isnumeric():
+      number_address += i
+  # Does anything else need to be done 
+  # before returning the result?
+  street_address = " ".join(split_string[1:])
+  # Return the formatted string  
+  return "house number {} on street named {}".format(number_address, street_address)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
