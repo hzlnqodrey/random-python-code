e1978a0be1e586249281237e3d748a3ae6e72188
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-09 02:31:23 +0700

    # Module 4 Graded Assessment: Q7 Done

--- app.py
+++ app.py
@@ -1648,3 +1648,29 @@ Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terr
 Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
 
 print(combine_guests(Rorys_guests, Taylors_guests))
+
+# Q7
+
+def count_letters(text):
+  result = {}
+  # Go through each letter in the text
+  for letter in text:
+    letter = letter.lower()
+    # Check if the letter needs to be counted or not
+    if (letter not in result):
+      result[letter] = 0
+      if not letter.isalpha():
+        del result[letter]
+        continue
+    # Add or increment the value in the dictionary
+    result[letter] += 1
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
