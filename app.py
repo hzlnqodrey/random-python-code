f425f6332ca5c8d551590ba67eb301e5fcabeabb
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-09 00:25:11 +0700

    Q2 Dict Done

--- app.py
+++ app.py
@@ -1531,3 +1531,19 @@ def email_list(domains):
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": [
       "barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for group, users in group_dictionary.items():
+		# Now go through the users in the group
+		for user in users:
+			# Now add the group to the the list of
+# groups for this user, creating the entry
+# in the dictionary if necessary
+			user_groups[user] = user_groups.get(user, []) + [group]
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
