7906f8438ebc2439fddf0730175f26176af38d9e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-09 01:40:13 +0700

    # Module 4 Graded Assessment: Q4 Done

--- app.py
+++ app.py
@@ -1616,3 +1616,12 @@ Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
 Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
 
 print(combine_lists(Jamies_list, Drews_list))
+
+# Q4
+
+def squares(start, end):
+	return [ x*x for x in range(start,end+1) ]
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
