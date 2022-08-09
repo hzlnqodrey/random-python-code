e7daaa44fb8e5bb6092e915e9db24f001a90c925
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-09 13:14:01 +0700

    Python OOP - dir function

--- app.py
+++ app.py
@@ -1674,3 +1674,10 @@ print(count_letters("Math is fun! 2+2=4"))
 
 print(count_letters("This is a sentence."))
 # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
+
+############################################################################
+
+
+# OOP
+
+dir("") # to check attributes and method inside the type
