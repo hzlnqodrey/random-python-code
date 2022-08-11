5d24510171e68cf53e4a093dcc9231c55015ee52
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-11 23:06:40 +0700

    Short Quiz: Dog

--- app.py
+++ app.py
@@ -2198,3 +2198,12 @@ print(piggy.pig_years()) # 0
 
 piggy.years = 2
 print(piggy.pig_years()) # 36
+
+class Dog:
+  years = 0
+  def dog_years(self):
+    return self.years * 7
+    
+fido = Dog()
+fido.years = 3
+print(fido.dog_years())
