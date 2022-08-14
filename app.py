fdf804e2acd9f41d0c97a903de671dc180714745
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-14 14:34:50 +0700

    Short Quiz: Inheritance

--- app.py
+++ app.py
@@ -2505,3 +2505,25 @@ class Cow(Animal):
 # Instance of the child class
 milky = Cow("Milky Whitey")
 milky.speak()
+
+# Short Quiz
+# Let’s create a new class together and inherit from it. Below we have a base class called Clothing. Together, let’s create a second class, called Shirt, that inherits methods from the Clothing class. Fill in the blanks to make it work properly.
+
+class Clothing:
+  material = ""
+  def __init__(self, name):
+    self.name = name
+  def checkmaterial(self):
+	  print("This {} is made of {}".format(self.name,self.material))
+			
+class Shirt(Clothing):
+  material="Cotton"
+
+polo = Shirt("Polo")
+polo.checkmaterial()
+
+# Here is your output:
+# This Polo is made of Cotton
+
+# Nice work! You used the existing Clothing class to make a
+# Shirt class that inherited from it!
