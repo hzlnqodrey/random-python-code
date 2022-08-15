b375b91bb46f2e87f2b8d27c85de3885c539b930
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-15 22:00:28 +0700

    ext. module pip installation

--- app.sh
+++ app.sh
@@ -8,3 +8,5 @@ python --version
 # External modules are generally managed with a command line tool called 'pip'
 # pip is cross-platform tool to install, update, and remove external modules
 
+# to install web browser request external module, we need to type
+pip install requests
