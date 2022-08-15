16b79d1e38c54bd5f9830c3b5aa8e76734929556
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-15 22:31:55 +0700

    data frame actually print it like a table

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ visitors = [1234, 5232, 9223, 8856, 7752]
 errors = [23, 52, 62, 89, 12]
 
 df = pd.DataFrame( {"visitors": visitors, "errors": errors}, index=["Mon", "Tue", "Wed", "Thu", "Fri"] )
+
+print(df)
