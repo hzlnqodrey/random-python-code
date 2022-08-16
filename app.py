3b91d5dc2ca7abc683a5c0e28058bd4e3c511a6e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-17 02:31:50 +0700

    Health Check (of a PC) Script's

--- app.py
+++ app.py
@@ -33,3 +33,24 @@ psutil.cpu_percent(10)
 0.0
 psutil.cpu_percent(.1)
 0.0
+
+########## Health Check (of a PC) Script's
+
+#!/usr/bin/env python3
+import shutil
+import psutil
+
+# to check disk usage, return true if there's more than 20 percent free or false if it's less
+def check_disk_usage(disk):
+    disk_usage = shutil.disk_usage(disk)
+    free = disk_usage.free / disk_usage.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    cpu_usage = psutil.cpu_percent(1)
+    return cpu_usage < 75
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR!")
+else:
+    print("Everything is OK!")
