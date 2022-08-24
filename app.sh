4010b31b8afcdcabcdc06353e361bab2cb7f6f08
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-24 18:44:05 +0700

    os.mkdir()

--- app.sh
+++ app.sh
@@ -167,7 +167,16 @@ qodri123@DESKTOP-V1LR167:/mnt/c/Windows/system32/programming_exercise$ ./health_
 Everything is OK!
 qodri123@DESKTOP-V1LR167:/mnt/c/Windows/system32/programming_exercise$
 
+# mkdir
 qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise$ mkdir folder1
 qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise$ ls
 __pycache__  areas.py  folder1  health_check.py  hello_world.py
 qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise$
+
+# mkdir in python (using os.mkdir)
+>>> import os
+>>> os.mkdir("new_dir")
+>>>
+[1]+  Stopped                 python3
+qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise$ ls
+__pycache__  areas.py  folder1  health_check.py  hello_world.py  new_dir
