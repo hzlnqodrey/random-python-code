e25756f393a312e87e82864702a2cc36aff2951e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-25 16:28:10 +0700

    fixing error and indent

--- Labs/2 - Handling Files/generate_report.py
+++ Labs/2 - Handling Files/generate_report.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python3
-
 import csv
 
 def read_employees(csv_file_location):
-  keys = ["Full Name", "Username", "Departement"]
-  with open(csv_file_location) as filename:
     csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-    employee_file = csv.DictReader(filename, fieldnames=keys, dialect='empDialect')
+    employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
 
     employee_list = []
-
     for data in employee_file:
         employee_list.append(data)
-    
-  return employee_list
+    return employee_list
 
-employee_list = read_employees('/home/student-04-2675d58b4395/user/data/employees.csv')
+employee_list = read_employees('/home/student-04-2675d58b4395/data/employees.csv') # Delete the preceding space
 
 def process_data(employee_list):
     department_list = []
-
     for employee_data in employee_list:
-        department_list.append(employee_data["department"])
-
-# The department_list should now have a redundant list of all the department names. We now have to remove the redundancy and return a dictionary. We will return this dicationary in the format department:amount, where amount is the number of employees in that particular department.
+        department_list.append(employee_data['Department'])
 
     department_data = {}
-
-    # This uses the set() method, which converts iterable elements to distinct elements.
     for department_name in set(department_list):
         department_data[department_name] = department_list.count(department_name)
+    return department_data
 
 dictionary = process_data(employee_list)
 print(dictionary)
