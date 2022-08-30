b758213951418d9ca188d0556cd08cd3c1f3b034
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-31 02:09:51 +0700

    Short Quiz: Simple Matching in Python

--- app.py
+++ app.py
@@ -402,6 +402,23 @@ for w in word:
   if comp:
     print(comp)
 
+# Short Quiz: Simple Matching in Python
+# Fill in the code to check if the text passed contains the vowels a, e and i, with exactly one occurrence of any other character in between.
+import re
+def check_aei(text):
+  result = re.search(r"a.e.i.", text)
+  return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True
+
+# Here is your output:
+# True
+# False
+# True
+
+# Great work! You've written your first regular expression!
 
 
 
