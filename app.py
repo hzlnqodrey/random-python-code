a36378896d5c787605f43e116df6e7b40d763be3
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-05 02:07:08 +0700

    perfecting the middle name regex searching

--- app.py
+++ app.py
@@ -769,7 +769,7 @@ rearrange_name("Hansel, Timotius") # 'Timotius Hansel'
 # Fix the regular expression used in the rearrange_name function so that it can match middle names, middle initials, as well as double surnames.
 import re
 def rearrange_name(name):
-  result = re.search(r"(\w*), (\w*\W.?\.)", name)
+  result = re.search(r"^([\w \.-]*), ([\w \.-]*)$", name)
   if result == None:
     return name
   return "{} {}".format(result[2], result[1])
