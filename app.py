4ff3a7955480b0d5fa890729567e0ed469201452
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-05 02:05:40 +0700

    adjust rearrange_name function to contain middle name matching

--- app.py
+++ app.py
@@ -762,3 +762,23 @@ def rearrange_name(name):
 rearrange_name("Lovelace, Ada") # 'Ada Lovelace'
 rearrange_name("Qodrey, Hazlan") # 'Hazlan Qodrey'
 rearrange_name("Hansel, Timotius") # 'Timotius Hansel'
+
+
+### Short Quiz: Capturing Groups
+# Question
+# Fix the regular expression used in the rearrange_name function so that it can match middle names, middle initials, as well as double surnames.
+import re
+def rearrange_name(name):
+  result = re.search(r"(\w*), (\w*\W.?\.)", name)
+  if result == None:
+    return name
+  return "{} {}".format(result[2], result[1])
+
+name=rearrange_name("Kennedy, John F.")
+print(name)
+
+# Here is your output:
+# John F. Kennedy
+
+# Nice work! You're doing well using regular expressions to
+# capture groups.
