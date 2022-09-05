52bdfd52f4df1d3367673058be45c17ef8e6b482
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-05 11:55:41 +0700

    Practice Quiz: Advanced Regular Expressions - Q1 Done

--- app.py
+++ app.py
@@ -923,3 +923,32 @@ re.split(r"the|a", "One sentence. Another one? And the last one!")
 
 # Correct
 # Well done! This regular expression uses "the" and "a" as delimiters, no matter where they are in the text, even in the middle of other words like "Another" and "last".
+
+
+### Practice Quiz: Advanced Regular Expressions
+
+# Question 1
+# We're working with a CSV file, which contains employee information. Each record has a name field, followed by a phone number field, and a role field. The phone number field contains U.S. phone numbers, and needs to be modified to the international format, with "+1-" in front of the phone number. Fill in the regular expression, using groups, to use the transform_record function to do that.
+
+import re
+def transform_record(record):
+  new_record = re.sub(r"([\w ]+),([\w -]+),([\w ]+)", r"\1,+1-\2,\3", record)
+  return new_record
+
+print(transform_record("Sabrina Green,802-867-5309,System Administrator")) 
+# Sabrina Green,+1-802-867-5309,System Administrator
+
+print(transform_record("Eli Jones,684-3481127,IT specialist")) 
+# Eli Jones,+1-684-3481127,IT specialist
+
+print(transform_record("Melody Daniels,846-687-7436,Programmer")) 
+# Melody Daniels,+1-846-687-7436,Programmer
+
+print(transform_record("Charlie Rivera,698-746-3357,Web Developer")) 
+# Charlie Rivera,+1-698-746-3357,Web Developer
+
+# Sabrina Green,+1-802-867-5309,System Administrator
+# Eli Jones,+1-684-3481127,IT specialist
+# Melody Daniels,+1-846-687-7436,Programmer
+# Charlie Rivera,+1-698-746-3357,Web Developer
+
