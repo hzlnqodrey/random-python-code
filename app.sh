bdbf0294fb267748fca4e6997d14d3f41deb5492
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-05 23:43:46 +0700

    Replacing 'abc.edu' to 'xyz.edu' done

--- app.sh
+++ app.sh
@@ -407,3 +407,28 @@ Jackson Owens, jackowens@abc.edu
 Britanni Humphrey, britanni@ut.net
 Kirk Nixon, kirknixon@abc.edu
 Bree Campbell, breee@utnisia.net
+
+## After Replacing 'abc.edu' to 'xyz.edu'
+
+student-04-97715962a5ea@linux-instance:~/data$ cat updated_user_emails.csv
+Full Name, Email Address
+Blossom Gill, blossom@xyz.edu
+Hayes Delgado, nonummy@utnisia.com
+Petra Jones, ac@xyz.edu
+Oleg Noel, noel@liberomauris.ca
+Ahmed Miller, ahmed.miller@nequenonquam.co.uk
+Macaulay Douglas, mdouglas@xyz.edu
+Aurora Grant, enim.non@xyz.edu
+Madison Mcintosh, mcintosh@nisiaenean.net
+Montana Powell, montanap@semmagna.org
+Rogan Robinson, rr.robinson@xyz.edu
+Simon Rivera, sri@xyz.edu
+Benedict Pacheco, bpacheco@xyz.edu
+Maisie Hendrix, mai.hendrix@xyz.edu
+Xaviera Gould, xlg@utnisia.net
+Oren Rollins, oren@semmagna.com
+Flavia Santiago, flavia@utnisia.net
+Jackson Owens, jackowens@xyz.edu
+Britanni Humphrey, britanni@ut.net
+Kirk Nixon, kirknixon@xyz.edu
+Bree Campbell, breee@utnisia.net
