2ac7ede6b72f90812f56e588e8792c662007202d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-06 00:28:08 +0700

    data type casting

--- app.sh
+++ app.sh
@@ -476,3 +476,22 @@ That's 363660 seconds
 Do you want to do another conversion? [y to continue] n
 Good bye!
 qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise/September$
+
+#!/usr/bin/env python3
+
+def to_seconds(hours, minutes, seconds):
+        return hours*3600+minutes*60+seconds
+
+print("Welcome to this thime converter program")
+
+cont = "y"
+while(cont.lower() == 'y'):
+        hours = int(input("Enter the number of hours: ")) # data type casting
+        minutes = int(input("Enter the number of minutes: ")) # data type casting
+        seconds = int(input("Enter the number of seconds: ")) # data type casting
+
+        print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+        print()
+        cont = input("Do you want to do another conversion? [y to continue] ")
+
+print("Good bye!")
