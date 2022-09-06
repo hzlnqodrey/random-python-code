7f737204314ac93c53a09f0c02d0c187a559128f
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-07 01:54:39 +0700

    ENV [environment variable in Shell/CLI]

--- app.sh
+++ app.sh
@@ -512,3 +512,27 @@ Traceback (most recent call last):
   File "./streams.py", line 5, in <module>
     print("Now we generate an error to STDERR" + data + 1)
 TypeError: can only concatenate str (not "int") to str
+
+####### ENV [environment variable in Shell/CLI]
+qodri123@DESKTOP-V1LR167:/mnt/c/WINDOWS/system32/programming_exercise/September/Reading-Data-interactively$ env
+SHELL=/bin/bash
+WSL_DISTRO_NAME=Ubuntu
+NAME=DESKTOP-V1LR167
+PWD=/mnt/c/WINDOWS/system32/programming_exercise/September/Reading-Data-interactively
+LOGNAME=qodri123
+MOTD_SHOWN=update-motd
+HOME=/home/qodri123
+LANG=C.UTF-8
+WSL_INTEROP=/run/WSL/11_interop
+LS_COLORS=rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:
+LESSCLOSE=/usr/bin/lesspipe %s %s
+TERM=xterm-256color
+LESSOPEN=| /usr/bin/lesspipe %s
+USER=qodri123
+SHLVL=1
+WSLENV=
+XDG_DATA_DIRS=/usr/local/share:/usr/share:/var/lib/snapd/desktop
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Python310/Scripts/:/mnt/c/Python310/:/mnt/c/Program Files (x86)/Common Files/Intel/Shared Libraries/redist/intel64/compiler:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/Program Files/NVIDIA Corporation/NVIDIA NvDLISR:/mnt/c/Program Files/Microsoft SQL Server/130/Tools/Binn/:/mnt/c/Program Files/Microsoft SQL Server/Client SDK/ODBC/170/Tools/Binn/:/mnt/c/Program Files/dotnet/:/mnt/c/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin:/mnt/c/Users/HAZLAN M QODRI/AppData/Roaming/Python/Python37/Scripts:/mnt/c/Program Files/Git/cmd:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/ProgramData/chocolatey/bin:/mnt/c/Program Files/nodejs/:/mnt/c/Program Files/MongoDB/Server/4.4/bin:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python39-32/Scripts:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python39-32:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python38/:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python38/Scripts/:/mnt/c/Program Files/Cloudflare/Cloudflare WARP/:/mnt/c/Program Files/Go/bin:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python39/Scripts/:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Python/Python39/:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/HAZLAN M QODRI/.dotnet/tools:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/GitHubDesktop/bin:/mnt/c/Users/HAZLAN M QODRI/AppData/Roaming/npm:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin:/mnt/c/Users/HAZLAN M QODRI/AppData/Local/atom/bin:/mnt/c/Program Files/JetBrains/PyCharm Community Edition 2022.2/bin:/mnt/c/Users/HAZLAN M QODRI/go/bin:/mnt/c/Program Files/heroku/bin:/snap/bin
+HOSTTYPE=x86_64
+OLDPWD=/mnt/c/WINDOWS/system32/programming_exercise/September
+_=/usr/bin/env
