3de1f815fb473d32bd82862732a9c16044cff757
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-14 06:02:45 +0700

    Additional Test Cases = def test_double_name(self):

--- app.py
+++ app.py
@@ -1429,3 +1429,35 @@ def rearrange_name(name):
         if result is None:
             return ""
         return "{} {}".format(result[2], result[1])
+
+
+### Additional Test Cases
+# we need to check further check of our regex, function, etc
+
+
+#!/usr/bin/env python3
+from rearrange import rearrange_name
+import unittest
+# inherit the TestCase class
+class TestRearrange(unittest.TestCase):
+    # ready to write first test case
+    def test_basic(self):
+        # we setting up our expected inputs and outputs
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+        # assertEqual to verify what we expected is exactly what we got
+
+    # we test empty string
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+    
+    # In this case, we're testing that someone with more than one given name still gets their name properly rearranged, Let's test it! 
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+unittest.main()
