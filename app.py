5c72c6ad68adf966404dad91757ec2a416c28ec0
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-14 07:22:06 +0700

    Practice Quiz: Other Test Concepts

--- app.py
+++ app.py
@@ -1596,3 +1596,108 @@ def rearrange_name(name):
 ### Continuous Integration [CI]
 # -> When engineers submit their code, it's integrated to main repository
 # -> and tests are automatically run againts it to spot bugs and error in a process
+
+## Practice Quiz: Other Test Concepts
+
+# Congratulations! You passed!
+# Grade received 100%
+# To pass 80% or higher
+# 1.
+# Question 1
+# In what type of test is the code not transparent?
+
+# 1 / 1 point
+
+# Correct Answer: Black-box test
+
+
+# White-box test
+
+
+# Test-driven development
+
+
+# Smoke test
+
+# Correct
+# Nice work! This type of test relies on the tester having no knowledge of the code.
+
+# 2.
+# Question 2
+# Verifying an automation script works well with the overall system and external entities describes what type of test?
+
+# 1 / 1 point
+
+# Correct Answer:  Integration test
+
+
+# Smoke test
+
+
+# Load test
+
+
+# Regression test
+
+# Correct
+# Great job! This test verifies that the different parts of the overall system interact as expected. 
+
+# 3.
+# Question 3
+# _____ ensures that any success or failure of a unit test is caused by the behavior of the unit in question, and doesn't result from some external factor.
+
+# 1 / 1 point
+
+# Regression testing
+
+
+# Integration
+
+
+# Correct Answer:  Isolation
+
+
+# White-box testing
+
+# Correct
+# Right on! By ensuring the unit of code we are testing is isolated, we can ensure we know where the bug originated.
+
+# 4.
+# Question 4
+# A test that is written after a bug has been identified in order to ensure the bug doesn't show up again later is called _____
+
+# 1 / 1 point
+
+# Load test
+
+
+# Black-box test
+
+
+# Smoke test
+
+
+# Correct Answer:  Regression test
+
+# Correct
+# Excellent! Regression testing is a type of software test used to confirm that a recent program or code change has not adversely affected existing features, by re-executing a full or partial selection test cases.
+
+# 5.
+# Question 5
+# What type of software testing is used to verify the software’s ability to behave well under significantly stressed testing conditions?
+
+# 1 / 1 point
+
+# Correct Answer:  Load test
+
+
+# Black-box test
+
+
+# Smoke test
+
+
+# Regression test
+
+# Correct
+# Way to go! Load testing verifies the behavior of the software remains consistent under conditions of significant load.
