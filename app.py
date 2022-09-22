45c60398f15c8c6e030fc3f875e28b5f5c0218e9
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-22 12:58:48 +0700

    Short Quiz: Redirecting Streams

--- app.py
+++ app.py
@@ -1857,3 +1857,22 @@ unittest.main()
 
 # STDIN
 ## < is to read the input
+
+## Short Quiz: Redirecting Streams
+
+# How do you append the output of a command to a .txt file?
+
+
+# user@ubuntu:~$ ./calculator.py > result.txt
+
+
+# CORRECT ANSWER: user@ubuntu:~$ ./calculator.py >> result.txt
+
+
+# user@ubuntu:~$ ./calculator.py < result.txt
+
+
+# user@ubuntu:~$ print("This will append")
+
+# Correct
+# Great work! A double greater than sign will append a command output to a file.
