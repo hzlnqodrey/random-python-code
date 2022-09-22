4abc348393b9f0beb8fba8d96eec28e9faa6eb4e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-22 10:32:53 +0700

    fixing error

--- app.py
+++ app.py
@@ -1770,7 +1770,7 @@ from validations import validate_user
 
 class TestValidateUser(unittest.TestCase):
         def test_valid(self):
-                self.assertEqual(validate_user("validuser, 3), True)
+                self.assertEqual(validate_user("validuser", 3), True)
 
         def test_too_short(self):
                 self.assertEqual(validate_user("inv", 5), False)
