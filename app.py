89f24bc3f1d4dc5f72a158e4dcf250b312219c2d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-23 14:29:12 +0700

    Short Quiz: Advanced Command Interaction

--- app.py
+++ app.py
@@ -2128,5 +2128,24 @@ sys.exit(value)
 
 ### Advanced Command Interaction
 
+# Short Quiz: Advanced Command Interaction
 
+# When using the following command, what would each line of the output start with?
+
+# user@ubuntu:~$ tail /var/log/syslog | cut -d' ' -f3-
+
+
+# CRON[257236]:
+
+
+# October
+
+
+# 31
+
+
+# Correct Answer: 10:18:41
+
+# Correct
+# Woohoo! The time of the log will be shown with the -f3- or field three option of the cut command.
 
